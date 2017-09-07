b15417d6892376fda97df7e9436c1c9926841667
Katja Kaiser <kddlpps@Katjas-MacBook-Pro.local>
2017-09-07 16:33:48 +0200

    initial commit

---
+++ AR_project/hello_world/hello_world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello_world
+//
+//  Created by Katja Kaiser on 07.09.17.
+//  Copyright © 2017 Katja Kaiser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
